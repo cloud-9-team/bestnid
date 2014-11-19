a460b35eac4dd9a9bd64ef21473e3facfbbac197
Emiliano Liotta <liotta.emiliano@gmail.com>
2014-11-19 17:57:10 -0300

    'Como usuario registrado quiero pujar por un producto' - Parte 1

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -1,5 +1,9 @@
 class BidsController < ApplicationController
+
+  before_action :authenticate_user!, only: [:index,:new,:destroy]
+
   def index
+    @bids = Bid.where(user_id: current_user.id)
   end
 
   def show
@@ -9,6 +13,18 @@ class BidsController < ApplicationController
   end
 
   def new
+    @product = Product.find(params[:product_id_param])
+    bid = @product.bids.new
+    bid.need = params[:need]
+    bid.value = params[:value]
+    bid.user_id = current_user.id
+    bid.save
+    flash[:notice]="Su oferta fue cargada correctamente."
+    redirect_to(bids_path)
+  end
+
+  def create
+
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   resources :answers
   resources :sales
   
+  post 'bids/new'
+
   get 'contacto/index'
 
   get 'paginas_estaticas/sobre_nosotros'
