3ed8f08535fc7599d4aa48d0055c592838bf7ece
Lucas Hourquebie <lucashour1993@gmail.com>
2014-11-19 13:33:15 -0300

    Publicar producto - parte 2

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,8 +24,7 @@ class ProductsController < ApplicationController
     i = params[:imageURL]
     d = params[:description]
     c = params[:totalDays]
-    u = current_user.id
-    Product.create(title:t,description:d,imageURL:i,totalDays:c,visitCount: 0,user:u,finished: false)
+    Product.create(title:t,description:d,imageURL:i,totalDays:c,visitCount: 0,user:current_user,finished: false)
     redirect_to welcome_index_path
   end
 
