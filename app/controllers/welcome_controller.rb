3e7853ae8ad1552e3d87970f916d582769b712cf
Federico Bouche <federicobouche@gmail.com>
2014-11-20 03:12:28 -0300

    Es posible ordenar productos

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,5 +5,21 @@ class WelcomeController < ApplicationController
 	else
 		@products = Product.all 
   	end
+
+  	if params[:sort_by].blank? == false
+  		case params[:sort_by].downcase
+	  		when "title"
+	  			@products = @products.by_title(params[:order])
+	  		when "created_at"
+	  			@products = @products.by_created_at(params[:order])
+	  		when "ends_at"
+	  			@products = @products.by_ends_at(params[:order])
+  		end
+  	elsif params[:order].blank? == false
+  		@products = @products.by_created_at(params[:order])
+  	else
+  		@products = @products.by_created_at("desc")
+  	end
+
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,10 +4,18 @@ class Product < ActiveRecord::Base
 	has_many :questions, dependent: :destroy
 	has_many :bids, dependent: :destroy
 
-	default_scope -> { order(created_at: :desc) }
+
+	scope :by_title, -> sentido { order(title: (((sentido.present?) and (sentido.downcase == "asc")) ? :asc : :desc))}
+	scope :by_created_at, -> sentido { order(created_at: (((sentido.present?) and (sentido.downcase == "asc")) ? :asc : :desc))}
+	#No funciona actualmente, necesidad de columna ends_at para mucha más facilidad. Analizar eliminación de totalDays
+	scope :by_ends_at, -> sentido { order("created_at " + (((sentido.present?) and (sentido.downcase == "asc")) ? "asc" : "desc").to_s )}
+
 	
 	def self.search(query)
-		where("title ilike ? or description ilike ?", "%#{query}%", "%#{query}%") | joins(:category).where("name ilike ?", "%#{query}%")
+		consulta1 = where('products.title ilike ? or products.description ilike ?', "%#{query}%", "%#{query}%")
+		consulta2 = joins(:category).where("categories.name ilike ?", "%#{query}%")
+		arr = consulta1 | consulta2 # Devuelve la unión de los dos conjuntos, pero es un arreglo, NO ES UNA ActiveRecord Relation
+		return where(id: arr.map(&:id)) #Conversión a AR Relation... no es lo mejor pero así permite usar scopes
 	end
 
 end
