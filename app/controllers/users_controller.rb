bb94aa6620bbea75c46bc0c93cbd8a1e4870b890
Emiliano Liotta <liotta.emiliano@gmail.com>
2014-11-21 19:25:47 -0300

    Se arregló la modificación de los datos de la tarjeta en la Oferta

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,6 +32,17 @@ def new
     end
   end
 
+  def update
+    usuario = current_user
+    usuario.card_1 = params[:user][:card_1]
+    usuario.card_2 = params[:user][:card_2]
+    usuario.card_3 = params[:user][:card_3]
+    usuario.card_4 = params[:user][:card_4]
+    usuario.save
+    flash[:notice] = "Los datos de la tarjeta de crédito se actualizaron correctamente."
+    redirect_to product_path(params[:product_id])
+  end
+
   def show
   	@user = User.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -34,7 +34,7 @@ Rails.application.routes.draw do
   post 'products/form'
 
   devise_for :users, :controllers => { registrations: 'registrations' }
-  resources :users, :only => [:show]
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
