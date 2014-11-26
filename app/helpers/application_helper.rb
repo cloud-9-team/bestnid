666844f3f64a6522fa74a4ae3c47a3a5d0ee487f
Federico Bouche <federicobouche@gmail.com>
2014-11-26 01:31:40 -0300

    Cambios menores en la vista del date_select

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,12 @@ module ApplicationHelper
 		options[:onerror] = "this.onerror=null; this.src='" + path_to_image("missing.png") + "';"
 		image_tag(source, options)
 	end
+
+	# Devuelve un arreglo con los nombres de los meses traducidos, con la primera letra en MAYÚSCULA
+	# Si use_short_month es true entonces entonces los nombres de los meses se devolverán abreviados,
+	# y para cualquier otro valor del parámetro se obtendrán los nombres completos.
+	def translated_month_names(use_short_month)
+        key = use_short_month ? :'date.abbr_month_names' : :'date.month_names'
+        I18n.translate(key).map {|month| month.present? ? month.titleize : nil}
+    end
 end
