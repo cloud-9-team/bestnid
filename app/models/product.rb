908f650234091cf4b8b684f623c371e3919e478f
Federico Bouche <federicobouche@gmail.com>
2014-11-25 22:22:51 -0300

    Vista de ventas de un usuario modificada

--- app/models/product.rb
+++ app/models/product.rb
@@ -20,4 +20,31 @@ class Product < ActiveRecord::Base
 		return where(id: arr.map(&:id)) #Conversión a AR Relation... no es lo mejor pero así permite usar scopes
 	end
 
+	# Permite saber si ya ha transcurrido el tiempo de la subasta.
+	# No indica nada si se ha escogido una oferta o no, sólo hace la comprobación de que el
+	# momento actual sea más tardío que el momento especificado para el final de la subasta,
+	# si es así, entonces este método devolverá true, de lo contrario false.
+	def timeout?
+		return Time.now > ends_at
+	end
+
+	# Permite saber si la subasta ha finalizado completamente, es decir que si ha transcurrido
+	# el tiempo de la subasta y se ha escogido una de todas las ofertas que se le han hecho al
+	# producto.
+	# Devuelve true si la subasta terminó, en cualquier otro caso false.
+	def finished?
+		if timeout? and chosen_bid.present?
+			return true
+		else
+			return false
+		end
+	end
+
+	# Permite obtener las preguntas que actualmente no tienen una respuesta asociada.
+	# El tipo devuelto es una ActiveRecord::Relation por lo cual es encadenable con
+	# otras consultas de la misma índole, por ejemplo métodos de ActiveRecord::QueryMethods.
+	def not_answered_questions
+		questions.includes(:answer).where(answers: {id: nil}).references(:answer)
+	end
+
 end
