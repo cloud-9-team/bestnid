5af6dd5f1e35a1447a6973e6aa13d53f2cf458bb
Federico Bouche <federicobouche@gmail.com>
2014-11-25 22:22:00 -0300

    Imagen alternativa de producto agregada

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,10 @@
 module ApplicationHelper
 	def timeleft(t)
-
 		content_tag :span, localize(t, format: :long_wo_year), title: localize(t, format: :db)
+	end
 
+	def image_tag_with_alt(source, options={})
+		options[:onerror] = "this.onerror=null; this.src='" + path_to_image("missing.png") + "';"
+		image_tag(source, options)
 	end
 end
