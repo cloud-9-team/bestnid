9fffb4be82f5ed0c8a380342964fe713e58c0aee
Federico Bouche <federicobouche@gmail.com>
2014-11-27 18:14:11 -0300

    Agregado método para obtener el nombre completo del país

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,12 @@ class User < ActiveRecord::Base
     has_many :products, dependent: :destroy
     has_many :questions
     has_many :bids, dependent: :destroy
+
+
+  	# A partir del código de país se intentará obtener el nombre completo
+  	# traducido, en caso de que no se pueda, se obtendrá su nombre en inglés.
+  	def country_name
+	    c = ISO3166::Country[country]
+	    c.translations[I18n.locale.to_s] || c.name
+  	end
 end
