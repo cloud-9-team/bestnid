1061752d1afaeae061047787c794a5e3162882eb
Emiliano Liotta <liotta.emiliano@gmail.com>
2014-11-27 11:14:38 -0300

    Se agregaron validaciones en el registro de usuario, la creacion de productos y las ofertas.

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,4 +1,8 @@
 class Bid < ActiveRecord::Base
+  include ActiveModel::Validations
 	belongs_to :product
 	belongs_to :user
+
+	validates :need, :allow_blank => false, :allow_nil => false, :on => :create, presence: true
+	validates :value, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,10 @@ class User < ActiveRecord::Base
     devise :database_authenticatable, :registerable,
            :recoverable, :rememberable, :trackable, :validatable
 
-    validates :country, :province, :city, :domicile, presence: true
+    validates :country, :province, :city, :domicile, :first_name, :email, :last_name, :allow_blank => false, :allow_nil => false, :on => :create, presence: true
+    validates :card_owner_first_name, :card_owner_last_name, :allow_blank => false, :allow_nil => false, :on => :create, presence: true
+    validates :security_code, :allow_blank => false, :length => { :minimum => 3, :maximum => 3 }
+    validates :card_1, :card_2, :card_3, :card_4, :allow_blank => false, :length => { :minimum => 4, :maximum => 4 }
 
     has_many :products, dependent: :destroy
     has_many :questions
