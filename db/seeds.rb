11f84c8a94ad7b3c0cc443d5ce96572b3d74e1a4
Federico Bouche <federicobouche@gmail.com>
2014-11-27 18:14:53 -0300

    Seeds configurados para elegir oferta ganadora

--- db/seeds.rb
+++ db/seeds.rb
@@ -174,6 +174,15 @@ aceite = vendedor3.products.find_or_create_by(title: "Aceite y Vinagre",
 	created_at: "2014-11-07 08:00:00",
 	ends_at: "2014-11-22 08:00:00")
 
+nutria = vendedor1.products.find_or_create_by(title: "Nutria",
+	description: "Las nutrias son adorables criaturas que viven tanto en la tierra como en el agua y a menudo se les confunde con los castores.\n
+				  Viven, principalmente, en tierras cerca de cuerpos de agua, y cazan su comida tanto en el agua, incluyendo a los peces, como en tierra, incluidas las aves.",
+	imageURL: "http://wikifaunia.com/images/3/3b/Nutria1.jpg",
+	visitCount: 0,
+	category: c3,
+	created_at: "2014-11-10 08:00:00",
+	ends_at: "2014-11-25 08:00:00")
+
 comprador1.bids.find_or_create_by(product: guante,
 	need: "Soy carnicero y lamentablemente perdí un dedo trabajando. Me gustaría comprar el producto para que no me vuelva a ocurrir.",
 	value: 50)
@@ -192,6 +201,22 @@ comprador3.bids.find_or_create_by(product: kriptonita,
 
 comprador4.bids.find_or_create_by(product: guante,
 	need: "Soy carpintero y la necesito para cuando manejo la sierra.",
+	value: 5000)
+
+comprador1.bids.find_or_create_by(product: nutria,
+	need: "Unos de mis objetivos en la vida es tener una nutria. Esta nutria hará feliz a una persona.",
+	value: 3)
+
+comprador2.bids.find_or_create_by(product: nutria,
+	need: "Necesito una nutria para que juegue con mis hijos. La pidieron para navidad",
+	value: 500)
+
+comprador3.bids.find_or_create_by(product: nutria,
+	need: "Tengo un nutrio y necesita compañia, ayudalo por favor",
+	value: 200)
+
+comprador4.bids.find_or_create_by(product: nutria,
+	need: "Es ideal para el tapado de nutria que ando buscando",
 	value: 5000)
 
 p1 = comprador3.questions.find_or_create_by(product: kriptonita,
