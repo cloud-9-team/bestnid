dcfc047818372d6b71d87acaea0449df36b0d9bf
Emiliano Liotta <liotta.emiliano@gmail.com>
2014-11-28 13:36:39 -0300

    Se añadió la verificación con password al eliminar cuenta.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-before_action :authenticate_user!, only: [:index, :show]
+before_action :authenticate_user!, only: [:index, :show, :destroy]
 	
   def index
   	@users = User.all
@@ -57,4 +57,17 @@ before_action :authenticate_user!, only: [:index, :show]
   def show
   	@user = User.find(params[:id])
   end
+
+  def destroy
+    user = current_user
+    if user.authenticate!(params[:password])
+         user.destroy
+         flash[:notice] = "La cuenta fue cerrada."
+         redirect_to welcome_index_path
+    else
+         flash[:alert] = "Contraseña incorrecta. Cuenta NO eliminada."
+         redirect_to welcome_index_path
+    end
+  end
+
 end
